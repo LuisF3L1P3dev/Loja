07c06ea3b0ff03c2e6e175f008836d54be43ed94
Luis Felipe <dantas.luis@escolar.ifrn.edu.br>
2025-01-30 21:42:33 -0300

    foi criado o validator para quantidade ser maior que zero e quantidade ser um número inteiro

--- apps/paginas/forms.py
+++ apps/paginas/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Fornecedor, Produto, Categoria
 from django.core.validators import ValidationError, MinValueValidator
-from .validators import validar_preco_positivo
+from .validators import validar_preco_positivo, quantidade_estoque_inteiro_positivo
 
 class CadastroFornecedoresForms(forms.ModelForm):
     class Meta:
@@ -10,7 +10,8 @@ class CadastroFornecedoresForms(forms.ModelForm):
         
 class CadastroProdutosForms(forms.ModelForm):
     preco = forms.DecimalField(validators=[validar_preco_positivo])
-    
+    quantidade = forms.IntegerField(validators=[quantidade_estoque_inteiro_positivo])    
+
     class Meta:
         model = Produto
         fields = '__all__'
--- apps/paginas/validators.py
+++ apps/paginas/validators.py
@@ -3,3 +3,9 @@ from django.core.validators import ValidationError
 def validar_preco_positivo(valor):
   if valor <= 0:
     raise ValidationError('O preço deve ser maior que zero')
+  
+def quantidade_estoque_inteiro_positivo(valor):
+  if not isinstance(valor, int): #isinstance permite vericar o tipo de dado do objeto, nesse caso se o valor é um inteiro
+    raise ValidationError('A quantidade deve ser um número inteiro')
+  if valor <= 0:
+    raise ValidationError('A quantidade deve ser maior que zero')
